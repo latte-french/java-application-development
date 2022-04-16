74228d55d63ea38bcdd948c1486f9ae3e12bb99d
latte_french <lexgolod@gmail.com>
2022-04-16 03:11:54 +0300

    Finished test in Iteration 1

--- src/main/java/com/acme/dbo/txlog/Decorator.java
+++ src/main/java/com/acme/dbo/txlog/Decorator.java
@@ -2,14 +2,14 @@ package com.acme.dbo.txlog;
 
 public class Decorator {
 
-    private static final String INT_PREFIX = "primitive: ";
+    private static final String PRIMITIVE_PREFIX = "primitive: ";
     private static final String CHAR_PREFIX = "char: ";
     private static final String STRING_PREFIX = "string: ";
 
     private static final String OBJECT_PREFIX = "reference: ";
 
     public static String decorateNumber (int message) {
-        return INT_PREFIX + message;
+        return PRIMITIVE_PREFIX + message;
     }
     public static String decorateChar (char message) {
         return CHAR_PREFIX + message;
@@ -20,7 +20,7 @@ public class Decorator {
     }
 
     public static String decorateBoolean (boolean message) {
-        return INT_PREFIX + message;
+        return PRIMITIVE_PREFIX + message;
     }
 
     public static String decorateObject (Object message) {
